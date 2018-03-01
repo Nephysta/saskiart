855c63a365c489f7b5a61fc1b7f4b3ddab3afa6e
nephysta <fanny.nephysta@gmail.com>
2018-03-01 17:51:19 +0100

    create method attach_picture

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -3,4 +3,11 @@ class Theme
     info = ThemeService.compose
     @ideas = info[:ideas]
   end
+
+  def attach_picture(picture)
+    @ideas.each do |idea|
+      idea.pictures << picture
+      idea.save
+    end
+  end
 end
