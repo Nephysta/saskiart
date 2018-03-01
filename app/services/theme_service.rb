366d3afdb7c0f7f65a8d0432bc2e0462affca2ca
nephysta <fanny.nephysta@gmail.com>
2018-03-01 20:50:48 +0100

    Theme.new can recieve a picture

--- app/services/theme_service.rb
+++ app/services/theme_service.rb
@@ -1,12 +1,7 @@
 module ThemeService
   class << self
     def compose
-      ideas = random_ideas
-
-      {
-        ideas: ideas,
-        text: format_ideas(ideas)
-      }
+      random_ideas
     end
 
     private
@@ -16,10 +11,6 @@ module ThemeService
         Idea.where(category: 'object').order('RANDOM()').limit(1).first,
         Idea.where(category: 'action').order('RANDOM()').limit(1).first
       ]
-    end
-
-    def format_ideas(ideas)
-      "#{ideas.map(&:text).join(' ')}.".capitalize
     end
   end
 end
